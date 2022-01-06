503c783c10b24f0abe2330792778e10c6830774c
Rudá Torquato <rudatorquato@gmail.com>
2022-01-06 11:41:15 -0300

    Adicionar TabBar na Parte Inferior

--- lib/favorite/widgets/tabs/tabs_widget.dart
+++ lib/favorite/widgets/tabs/tabs_widget.dart
@@ -2,10 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:meals/home/widgets/home_page_widget.dart';
 import '../favorite/favorite_widget.dart';
 
-class TabsWidget extends StatelessWidget {
+class TabsWidget extends StatefulWidget {
   const TabsWidget({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<TabsWidget> createState() => _TabsWidgetState();
+}
+
+class _TabsWidgetState extends State<TabsWidget> {
+  int selectedWidgetIndex = 0;
+  final List<String> titles = [
+    'Lista de Categorias',
+    'Meus Favoritos',
+  ];
+  final List<Widget> screens = [
+    const Categories(),
+    const FavoriteWidget(),
+  ];
+
+  _selectWidget(int index) {
+    setState(() {
+      selectedWidgetIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,27 +34,31 @@ class TabsWidget extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: const Center(
-            child: Text('Vamos cozinhar?'),
-          ),
-          bottom: const TabBar(
-            indicatorColor: Colors.amber,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritos',
-              )
-            ],
+          title: Center(
+            child: Text(
+              titles[selectedWidgetIndex],
+            ),
           ),
         ),
-        body: const TabBarView(
-          children: [
-            Categories(),
-            FavoriteWidget(),
+        body: screens[selectedWidgetIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          //backgroundColor: Colors.pink,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Colors.amber,
+          currentIndex: selectedWidgetIndex,
+          type: BottomNavigationBarType.shifting,
+          onTap: _selectWidget,
+          items: const [
+            BottomNavigationBarItem(
+              backgroundColor: Colors.pink,
+              icon: Icon(Icons.category),
+              label: 'Categorias',
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Colors.pink,
+              icon: Icon(Icons.star),
+              label: 'Favoritos',
+            )
           ],
         ),
       ),
